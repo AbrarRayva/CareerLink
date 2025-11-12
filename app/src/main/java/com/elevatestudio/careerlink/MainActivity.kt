29eafda762891f6e1dbbabc2a8d92fc5cfb2bb70
abrar <abrarrayva.id@gmail.com>
2025-11-12 23:50:48 +0700

    fix: routing issues

--- app/src/main/java/com/elevatestudio/careerlink/MainActivity.kt
+++ app/src/main/java/com/elevatestudio/careerlink/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Panggil navigasi utama di sini
                     AppNavigation()
                 }
             }
--- app/src/main/java/com/elevatestudio/careerlink/ui/screen/kursus/DashboardKursusScreen.kt
+++ app/src/main/java/com/elevatestudio/careerlink/ui/screen/kursus/DashboardKursusScreen.kt
@@ -3,7 +3,6 @@ package com.elevatestudio.careerlink.ui.screen.kursus
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,7 +14,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,7 +21,6 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.elevatestudio.careerlink.data.model.BadgeItem
 import com.elevatestudio.careerlink.data.model.KursusItem
@@ -50,7 +47,7 @@ fun DashboardKursusScreen(
     onNavigateToDetailKursus: (String) -> Unit,
     onNavigateToBadgeScan: () -> Unit
 ) {
-    val username = "Siti Aliani" // Data dummy
+    val username = "User" // Data dummy
 
     var isVisible by remember { mutableStateOf(false) }
     LaunchedEffect(Unit) {
