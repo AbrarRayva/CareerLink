b1cc77dc5f82a39293f10befdb5d59d1f6c3e170
abrar <abrarrayva.id@gmail.com>
2025-12-25 20:42:27 +0700

    fix: course ui fix 2

--- app/src/main/java/com/elevatestudio/careerlink/ui/theme/Theme.kt
+++ app/src/main/java/com/elevatestudio/careerlink/ui/theme/Theme.kt
@@ -19,7 +19,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = PrimaryGreen,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
